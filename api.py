e3da2db575e84867d0e1d9d684b2debf33c67b52
ZeRoX4V <enzoforato@orange.fr>
2024-04-28 20:13:58 +0200

    Modif de la v1

--- api.py
+++ api.py
@@ -37,10 +37,12 @@ def decrypt_password(encrypted_password):
 # Dictionnaire pour save le fichier upload et le mot de passe
 file_storage = {}
 
+
+
 # Affiche la page index.html
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("upload.html", {"request": request})
 
 # Affiche la pahe upload.html
 @app.get("/upload", response_class=HTMLResponse)
@@ -50,7 +52,6 @@ async def upload_page(request: Request):
 # Fonction pour upload un fichier
 @app.post("/upload/")
 async def upload_file(request: Request, file: Optional[UploadFile] = File(...), password: str = Form(...)):
-    # Récupère le nom du fichier
     filename = file.filename
  
     # Générer un identifiant unique pour le fichier
@@ -63,10 +64,8 @@ async def upload_file(request: Request, file: Optional[UploadFile] = File(...), 
     if not os.path.exists("uploads"):
         os.makedirs("uploads")
 
-    # Créer le chemin du fichier
     file_path = os.path.join("uploads", encrypted_file_name)
     
-    # Sauvegarde le fichier
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
@@ -76,7 +75,6 @@ async def upload_file(request: Request, file: Optional[UploadFile] = File(...), 
     # Stocke l'identifiant du fichier chiffré avec le mot de passe chiffré et le nom du fichier chiffré
     file_storage[file_id] = {"file_path": file_path, "password": encrypted_password}
 
-    # Retourner l'identifiant du fichier pour être utilisé dans le modèle
     return templates.TemplateResponse("upload.html", {"request": request, "file_id": file_id})
 
 
@@ -90,35 +88,29 @@ async def download_page(request: Request, file_id: str):
 async def download_file(request: Request, file_id: str, password: str = Form(...)):
 
     try:
-        # Vérifier si l'identifiant de fichier existe dans le stockage
         if file_id in file_storage:
 
-            # Récupère les infos du fichier 
             file_info = file_storage[file_id]
-            # Récupère le chemin du fichier
             file_path = file_info["file_path"]
-            # Récupère le mot de passe lié au fichier
             stored_encrypted_password = file_info["password"]
             
             # Déchiffrement du mot de passe stocké
             stored_password = decrypt_password(stored_encrypted_password)
 
-            # Vérification du mot de passe fourni par l'utilisateur
             if password == stored_password:
 
-                # Extraire le nom de fichier original du chemin stocké
                 stored_decrypted_filename = os.path.basename(unquote(file_path))
                 file_name = decrypt_file_id(stored_decrypted_filename)
                 
-                # Retourner le fichier pour le téléchargement
                 return FileResponse(file_path, headers={"Content-Disposition": f"attachment; filename={file_name}"})
             else:
-                # Mot de passe incorrect
                 return templates.TemplateResponse("download.html", {"request": request, "file_id": file_id, "error_message": "Invalid password."})
         else:
-            # Fichier non trouvé
             return templates.TemplateResponse("download.html", {"request": request, "file_id": file_id, "error_message": "File not found."})
     except InvalidToken:
-        # Token Fernet invalide
         raise HTTPException(status_code=400, detail="Invalid token")
 
+# Affiche la pahe upload.html
+@app.get("/upload_bdd", response_class=HTMLResponse)
+async def upload_page(request: Request):
+    return templates.TemplateResponse("upload_bdd.html", {"request": request})
